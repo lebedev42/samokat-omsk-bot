0bcd8ab3fbf0a0da373f535ad28accc2feeab30a
lebedev42 <leb.dev42@gmail.com>
2023-11-21 18:57:10 +0300

    Added running host -2

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,6 @@ app.post("/api/sendAnswer", async (req, res) => {
   res.status(200).end();
 });
 
-app.listen(process.env.PORT, "localhost", () => {
+app.listen("localhost", process.env.PORT () => {
   console.log("Server started", process.env.PORT);
 });
